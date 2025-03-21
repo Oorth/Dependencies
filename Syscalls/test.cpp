bedb63445129e4708ef8edb2033c570da108b622
Oorth <arthmaurya@gmail.com>
2025-03-22 00:03:53 +0530

    got ssn

---
+++ Syscalls/test.cpp
@@ -0,0 +1,88 @@
+//cl.exe /EHsc .\test.cpp /link /OUT:test.exe
+#define DEBUG 1
+
+#include <Windows.h>
+#include <iostream>
+
+////////////////////////////////////////////////////////////////////////////////
+#if DEBUG
+    #define ok(something) std::cout << " [+] " << something << std::endl;
+    #define fuk(something) std::cout << " [-] " << something << std::endl;
+#else
+    #define ok(something)
+    #define fuk(something)
+#endif
+////////////////////////////////////////////////////////////////////////////////
+
+HMODULE hNtdll = nullptr;
+
+////////////////////////////////////////////////////////////////////////////////
+
+void* FindExportAddress(HMODULE hModule, const char* funcName)
+{
+    IMAGE_DOS_HEADER* dosHeader = (IMAGE_DOS_HEADER*)hModule;
+    IMAGE_NT_HEADERS* ntHeaders = (IMAGE_NT_HEADERS*)((BYTE*)hModule + dosHeader->e_lfanew);
+
+    IMAGE_EXPORT_DIRECTORY* exportDir = (IMAGE_EXPORT_DIRECTORY*)((BYTE*)hModule + ntHeaders->OptionalHeader.DataDirectory[0].VirtualAddress);
+
+    DWORD* nameRVAs = (DWORD*)((BYTE*)hModule + exportDir->AddressOfNames);
+    WORD* ordRVAs = (WORD*)((BYTE*)hModule + exportDir->AddressOfNameOrdinals);
+    DWORD* funcRVAs = (DWORD*)((BYTE*)hModule + exportDir->AddressOfFunctions);
+    for (DWORD i = 0; i < exportDir->NumberOfNames; ++i)
+    {
+        char* funcNameFromExport = (char*)((BYTE*)hModule + nameRVAs[i]);
+        if (strcmp(funcNameFromExport, funcName) == 0)
+        {
+            DWORD funcRVA = funcRVAs[ordRVAs[i]];
+            return (void*)((BYTE*)hModule + funcRVA);
+        }
+    }
+    std::cout << "Failed to find export address of: " << funcName << "\tGetlastError message -> " << GetLastError() << std::endl;
+    return nullptr;
+}
+
+DWORD FindSyscallSSN(const char* function_name)
+{
+    void* vpfunction = nullptr;
+    DWORD dSyscall_SSN = 0;
+
+    vpfunction = FindExportAddress(hNtdll, function_name);
+    if(!vpfunction)
+    {
+        fuk("Coudnt find the function");
+        return 0;
+    }
+
+    BYTE* pBytes = reinterpret_cast<BYTE*>(vpfunction);
+    if(pBytes[0] == 0x4C && pBytes[1] == 0x8B && pBytes[2] == 0xD1)
+    {
+        ok("Function is Unhooked");
+        dSyscall_SSN = *(DWORD*)(pBytes + 4);
+
+        return dSyscall_SSN;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    const char* function_name;
+    DWORD dSSN = 0;
+
+    hNtdll = LoadLibraryW(L"ntdll.dll");    
+    if(!hNtdll)
+    {
+        fuk("cant load ntdll");
+        return 1;
+    } ok("loaded ntdll");
+
+    dSSN = FindSyscallSSN("NtProtectVirtualMemory");
+    if(!dSSN)
+    {
+        fuk("Coudnt find the ssn");
+        return 1;
+    }
+    std::cout << "SSN : " << dSSN << std::endl;
+    return 0;
+}
