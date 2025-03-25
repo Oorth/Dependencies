d8f835cf0046b6a67eccfd90da539d8a112b739d
Oorth <arthmaurya@gmail.com>
2025-03-25 14:58:50 +0530

    Added colours and norm()

--- Debugging Macros/DbgMacros.cpp
+++ Debugging Macros/DbgMacros.cpp
@@ -6,19 +6,26 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #if DEBUG
-    #define ok(...) details::log(" [+] ", ##__VA_ARGS__)
-    #define fuk(...) details::log(" [-] ", ##__VA_ARGS__, " [!] ")
-    #define warn(...) details::log(" [!] ", ##__VA_ARGS__)
+    #define GREEN "\033[32m"
+    #define RED "\033[31m"
+    #define YELLOW "\033[33m"
+    #define RESET "\033[0m"
+    
+    #define ok(...) details::log(GREEN " [+] ", ##__VA_ARGS__)
+    #define fuk(...) details::log(RED " [-] ", ##__VA_ARGS__, " [!] ")
+    #define warn(...) details::log(YELLOW " [!] ", ##__VA_ARGS__)
+    #define norm(...) details::log("", ##__VA_ARGS__)
 #else
     #define ok(...)
     #define fuk(...)
     #define warn(...)
+    #define norm(...)
 #endif
 
 #if DEBUG
 namespace details
 {
-    void log_arg(std::ostream& os) {}
+    void log_arg(std::ostream& os) { os << RESET; }
 
     template <typename T, typename... Args>
     void log_arg(std::ostream& os, const T& arg, Args... args)
@@ -27,10 +34,7 @@ namespace details
         log_arg(os, args...);
     }
 
-    void log_arg(std::ostream& os, std::ostream& (*manip)(std::ostream&))
-    {
-        manip(os);
-    }
+    void log_arg(std::ostream& os, std::ostream& (*manip)(std::ostream&)) { manip(os); }
 
     template <typename T>
     void log(const char* prefix, const T& single_arg)
@@ -43,7 +47,7 @@ namespace details
     template <typename... Args>
     void log(const char* prefix, const char* msg, Args... args)
     {
-        std::cout << prefix << msg << " ";
+        std::cout << prefix << msg;
         log_arg(std::cout, args...);
         std::cout << std::endl;
     }
@@ -51,7 +55,7 @@ namespace details
     template <typename... Args>
     void log(const char* prefix, const char* msg, Args... args, const char* suffix)
     {
-        std::cout << prefix << msg << " ";
+        std::cout << prefix << msg;
         log_arg(std::cout, args...);
         std::cout << suffix << std::endl;
     }
@@ -62,8 +66,10 @@ int main()
 {
     void* a = (void*)10;
     int i = 1;
+
     ok(i);
     ok("just text");
+    norm("normal");
     ok("int -> ", std::dec, reinterpret_cast<intptr_t>(a), " and the Hex -> 0x", a );
     fuk("int -> ", std::dec, reinterpret_cast<intptr_t>(a), " and the Hex -> 0x", a );
     warn("int -> ", std::dec, reinterpret_cast<intptr_t>(a), " and the Hex -> 0x", a );
