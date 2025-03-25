7d48db66e4b92af51d2e77cd87bcd852443ff34f
Oorth <arthmaurya@gmail.com>
2025-03-25 16:19:31 +0530

    added vector

--- Debugging Macros/DbgMacros.h
+++ Debugging Macros/DbgMacros.h
@@ -2,6 +2,7 @@
  * Debugging Macros Usage:
  * 
  * !!!!Include a #define DEBUG 0/1 in your code!!!!
+ * This header may include iostream depending on the DEBUG
  * 
  * This file provides color-coded debugging macros for console output.
  * When DEBUG is set to 1, the following macros are available:
@@ -30,6 +31,12 @@
     #if DEBUG
         #include <iostream>
     #endif
+
+    #if log_vector
+        #include <vector>
+        #include <sstream>
+        #include <iomanip>
+    #endif
     ////////////////////////////////////////////////////////////////////////////////
 
     #if DEBUG
@@ -50,42 +57,68 @@
     #endif
 
     #if DEBUG
-    namespace details
-    {
-        void log_arg(std::ostream& os) { os << RESET; }
-
-        template <typename T, typename... Args>
-        void log_arg(std::ostream& os, const T& arg, Args... args)
+        namespace details
         {
-            os << arg;
-            log_arg(os, args...);
-        }
+            #if log_vector
+                std::vector<std::string> logged_messages;
+            #endif
 
-        void log_arg(std::ostream& os, std::ostream& (*manip)(std::ostream&)) { manip(os); }
+            void log_arg(std::ostream& os) { os << RESET; }
 
-        template <typename T>
-        void log(const char* prefix, const T& single_arg)
-        {
-            std::cout << prefix;
-            log_arg(std::cout, single_arg);
-            std::cout << std::endl;
-        }
+            template <typename T, typename... Args>
+            void log_arg(std::ostream& os, const T& arg, Args... args)
+            {
+                os << arg;
+                log_arg(os, args...);
+            }
 
-        template <typename... Args>
-        void log(const char* prefix, const char* msg, Args... args)
-        {
-            std::cout << prefix << msg;
-            log_arg(std::cout, args...);
-            std::cout << std::endl;
-        }
+            void log_arg(std::ostream& os, std::ostream& (*manip)(std::ostream&)) { manip(os); }
 
-        template <typename... Args>
-        void log(const char* prefix, const char* msg, Args... args, const char* suffix)
-        {
-            std::cout << prefix << msg;
-            log_arg(std::cout, args...);
-            std::cout << suffix << std::endl;
+            template <typename... Args>
+            void log(const char* prefix, Args... args)
+            {
+                std::cout << prefix;
+                log_arg(std::cout, args...);
+                std::cout << RESET << std::endl;
+                
+                #if log_vector
+                    std::stringstream ss;
+                    ss << prefix;
+                    log_arg(ss, args...);
+                    logged_messages.push_back(ss.str());
+                #endif
+            }
+
+            template <typename... Args>
+            void log(const char* prefix, const char* msg, Args... args)
+            {
+                std::cout << prefix << msg << " ";
+                log_arg(std::cout, args...);
+                std::cout << RESET << std::endl;
+                
+                #if log_vector
+                    std::stringstream ss;
+                    ss << prefix << msg << " ";
+                    log_arg(ss, args...);
+                    logged_messages.push_back(ss.str());
+                #endif
+            }
+
+            template <typename... Args>
+            void log(const char* prefix, const char* msg, Args... args, const char* suffix)
+            {
+                std::cout << prefix << msg << " ";
+                log_arg(std::cout, args...);
+                std::cout << suffix << RESET << std::endl;
+            
+                #if log_vector
+                    std::stringstream ss;
+                    ss << prefix << msg << " ";
+                    log_arg(ss, args...);
+                    ss << suffix;
+                    logged_messages.push_back(ss.str());
+                #endif
+            }
         }
-    }
     #endif
 #endif
--- Debugging Macros/testmacros.cpp
+++ Debugging Macros/testmacros.cpp
@@ -1,5 +1,6 @@
 //cl.exe /EHsc .\testmacros.cpp /link /OUT:testmacros.exe
 #define DEBUG 1
+#define log_vector 1
 #include "DbgMacros.h"
 
 int main()
@@ -13,6 +14,11 @@ int main()
     ok("int -> ", std::dec, reinterpret_cast<intptr_t>(a), " and the Hex -> 0x", a );
     fuk("int -> ", std::dec, reinterpret_cast<intptr_t>(a), " and the Hex -> 0x", a );
     warn("int -> ", std::dec, reinterpret_cast<intptr_t>(a), " and the Hex -> 0x", a );
+
+    #if log_vector
+        std::cout << "\nLogged messages in vector:\n";
+        for (const auto& msg : details::logged_messages) std::cout << msg << std::endl;
+    #endif
 
     return 0;
 }
